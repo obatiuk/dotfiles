ae951e0d8ac5ea0ee43882db2c9d5d1d5f4d3df7
Oleksii Batiuk <oleksii.batiuk@gmail.com>
2015-11-18 13:03:52 -0500

    cleanup

--- .i3/lock/lock.sh
+++ .i3/lock/lock.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
- 
+
 # Dependencies:
 # imagemagick
 # i3lock
 # scrot
- 
+
 IMAGE=/tmp/i3lock.png
- 
-BASEDIR=$(dirname $0)
- 
-BLURTYPE="0x8"
 
 scrot $IMAGE
-convert $IMAGE -blur $BLURTYPE -paint 10 $IMAGE
+convert $IMAGE -blur 0x8 -paint 10 $IMAGE
 i3lock -i $IMAGE
 rm $IMAGE
