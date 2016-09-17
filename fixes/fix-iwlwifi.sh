8e865b4b38f889ef381855d1951c6b0d263e102e
obatiuk <oleksii.batiuk@gmail.com>
2016-09-16 22:20:02 -0400

    Created folder for very specific fixes

---
+++ fixes/fix-iwlwifi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Fix for iwlwifi performance drop for "Intel Corporation Centrino Advanced-N 6235 (rev 24)" network controler
+#
+# Source:
+# - https://wireless.wiki.kernel.org/en/users/Drivers/iwlwifi#wifibluetooth_coexistence
+# - https://wiki.debian.org/iwlwifi
+
+echo options iwlwifi bt_coex_active=0 swcrypto=1 11n_disable=8 | sudo tee /etc/modprobe.d/iwlwifi.conf
+sudo modprobe -rfv iwldvm
+sudo modprobe -v iwlwifi
