ac9a7d72940109cbc9f77b95f694e0c4a0116248
Oleksii Batiuk <oleksii.batiuk@gmail.com>
2023-06-05 23:10:15 -0400

    Add script to install `ulauncher` extensions

---
+++ extra/install-ulauncher-extensions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Sanity checks
+command -v git > /dev/null 2>&1 || { echo "'git' command not found. Aborting!" && exit 1; }
+command -v python3 > /dev/null 2>&1 || { echo "'python3' command not found. Aborting!" && exit 1; }
+
+# make sure that required environment variables are set during installation
+: "${ULAUNCHER_EXTENSIONS:=${HOME}/.local/share/ulauncher/extensions}"
+: "${HOME_STORAGE:=${HOME}/.home}"
+: "${APP_STORAGE:=${HOME_STORAGE}/opt}"
+
+# download source code and install extensions via symlinks
+mkdir -p "${APP_STORAGE}"
+mkdir -p "${ULAUNCHER_EXTENSIONS}"
+
+declare -a __extension_map=(
+	'ulauncher ulauncher-emoji.git'
+	'yannishuber pass-ulauncher.git'
+	'caraterra pass-for-ulauncher.git'
+	'caraterra pass-otp-for-ulauncher.git'
+	'mikebarkmin ulauncher-obsidian.git'
+)
+for row in "${__extension_map[@]}"; do
+	read -a entry <<< "${row}"
+	git clone "https://github.com/${entry[0]}/${entry[1]}" "${APP_STORAGE}/${entry[1]}"
+	ln -s "${APP_STORAGE}/${entry[1]}" "${ULAUNCHER_EXTENSIONS}/com.github.${entry[0]}.${entry[1]//.git/}"
+done
